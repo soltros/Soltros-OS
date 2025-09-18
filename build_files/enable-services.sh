1939474b122048180a12b2de871603694d67f629
Derrik Diener <142091676+soltros@users.noreply.github.com>
2025-09-18 13:47:42 -0400

    Update enable-services.sh

--- build_files/enable-services.sh
+++ build_files/enable-services.sh
@@ -8,10 +8,5 @@ log() {
   echo "=== $* ==="
 }
 
-
-log "Enabling soltros audio resume service"
-chmod 755 /usr/libexec/soltros/soltros-resume-monitor.sh || true
-systemctl enable soltros-resume-fix.service
-
 log "Enabling Tailscale"
 systemctl enable tailscaled.service

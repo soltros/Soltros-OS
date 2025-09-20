03039edfcc4d0f4cff7f6c8900bd3ced1660749f
Derrik Diener <142091676+soltros@users.noreply.github.com>
2025-09-20 04:09:15 -0400

    Update desktop-packages.sh

--- build_files/desktop-packages.sh
+++ build_files/desktop-packages.sh
@@ -21,24 +21,15 @@ LAYERED_PACKAGES=(
     papirus-icon-theme
     papirus-icon-theme-dark
     papirus-icon-theme-light
-    materia*
-    kvantum
     qt5-qtgraphicaleffects
     lm_sensors
     udisks2
-    udiskie
-    gimp
     pipewire
-    pipewire-pulse
     wireplumber
-    nebula
     dbus-tools
     alsa-utils
     syslinux
     pipewire-alsa
-    deja-dup
-    playerctl
-    guestmount
     libguestfs-tools
     linux-firmware*
     pipewire-alsa 
@@ -62,35 +53,15 @@ LAYERED_PACKAGES=(
     mesa-libEGL
     mesa-libGL
     mesa-libgbm
-    mesa-va-drivers
     mesa-vulkan-drivers
-    fwupd
-    fwupd-plugin-flashrom
-    fwupd-plugin-modem-manager
-    fwupd-plugin-uefi-capsule-data
-    libvirtd
     konsole
     konsole-part
-    
-    # Display manager
     sddm
-    
-    # Gaming & performance
-    gamemode
-    gamemode-devel
-    mangohud
-    goverlay
-    corectrl
-    steam-devices
-    # MacBook thermal management
-    mbpfan
     thermald
     
     # Essential CLI tools
     btop
     ripgrep
-    fd-find
-    git-delta
     
     # System monitoring & hardware
     nvtop
@@ -120,13 +91,9 @@ LAYERED_PACKAGES=(
     gvfs-fuse
     gvfs-mtp
     gvfs-gphoto2
-    gvfs-archive
-    gvfs-afp
-    gvfs-nfs
     samba-client
     cifs-utils
     virt-manager
-    gnome-boxes
     gnome-tweaks
     
     # Multimedia/audio
